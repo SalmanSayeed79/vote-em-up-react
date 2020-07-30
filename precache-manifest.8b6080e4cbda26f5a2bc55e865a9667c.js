c1d502b7fdb828646e5317bcaffe29c0c8a6a50f
SalmanSayeed79 <66307068+SalmanSayeed79@users.noreply.github.com>
2020-07-30 22:09:32 +0600

    first build commit
    Problems:
    --------------------------------------
    -the welcome doesn't work
    -can give multiple votes
    -the bottom bar doesn't work

---
+++ precache-manifest.8b6080e4cbda26f5a2bc55e865a9667c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f3581a97c2812e240d9954bdbea8c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af072fc05fea78f12755",
+    "url": "/static/css/main.5fc52194.chunk.css"
+  },
+  {
+    "revision": "b99af84b41b11345e626",
+    "url": "/static/js/2.798cd34b.chunk.js"
+  },
+  {
+    "revision": "31b65397dc8a13083a81efedadb0f77a",
+    "url": "/static/js/2.798cd34b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af072fc05fea78f12755",
+    "url": "/static/js/main.eb9098c2.chunk.js"
+  },
+  {
+    "revision": "0001995d880b7250755d",
+    "url": "/static/js/runtime-main.aa701e89.js"
+  },
+  {
+    "revision": "bc5e575b028e9edd1d3ff3a017836036",
+    "url": "/static/media/home.bc5e575b.png"
+  }
+]);
